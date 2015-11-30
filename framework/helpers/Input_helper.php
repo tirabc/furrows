703c7e69318b0763c421846e8cc1b69c9b7c7c8c
Christian Barras <tirabc@gmail.com>
2015-11-30 16:25:22 +0100

    Add Input_helper

---
+++ framework/helpers/Input_helper.php
@@ -0,0 +1,42 @@
+<?php
+
+class Input
+{
+
+    static function filter( $args , $keys )
+    {
+        global $error;
+        $array = array();
+        foreach( $keys as $i => $v )
+        {
+            if( array_key_exists( $v , $args ) && !empty( $args[ $v ] ) )
+            {
+                $array[ $v ] = $args[ $v ];
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return $array;
+
+    }
+
+    static function sanitize( $field )
+    {
+        return htmlentities( htmlspecialchars( $field ) );
+    }
+
+    static function escape_mysql( $field )
+    {
+        return htmlentities( mysql_real_escape_string( $field ) );
+    }
+
+    static function hash( $field )
+    {
+        return md5( $field );
+    }
+
+}
+?>
